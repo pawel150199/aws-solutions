ea80c334ba107d3caf28f1fc33e974c8ed81bf8a
pawel150199 <pawel.polski99@gmail.com>
2024-12-24 00:00:49 +0100

    update step-function example

--- step-functions/README.md
+++ step-functions/README.md
@@ -1,2 +1,4 @@
 # Step Functions
-I was testing AWS Step Functions and for future I have decided to create some IaC as a notes how it works.
+I was learning AWS Step function and this code is for my learning purpose
+
+I have noticed that Step Functions are very good for automation the all pipeline processes like do some actions once in month like an example. It is very simple and reliable solution but you have to write lambdas for more complicated purposes. 
--- step-functions/lambda/main.py
+++ step-functions/lambda/main.py
@@ -9,40 +9,15 @@ sns = boto3.client('sns')
 
 def configure_logger():
     """Configure logger"""
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setFormatter(formatter)
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
-    logger.addHandler(console_handler)
     return logger
-
-
-def send_sns_notification(logger, s3_bucket, s3_filename):
-    """Send an SNS notification when the file is available in S3"""
-    sns_topic_arn = os.getenv('SNS_TOPIC_ARN')
-    if not sns_topic_arn:
-        raise ValueError("The SNS topic name is not set")
-    
-    message = f"The file '{s3_filename}' is now available in S3 bucket '{s3_bucket}'."
-    subject = "File available in S3"
-    
-    try:
-        sns.publish(
-            TopicArn=sns_topic_arn,
-            Message=message,
-            Subject=subject
-        )
-        logger.info("SNS notification has been successfully sent")
-    except ClientError as e:
-        logger.error(f"Failed to send SNS notification: {e}")
-        raise e
 
 
 def create_file(logger, s3_bucket, s3_filename, content):
     """Create example file in S3 bucket"""
     try:
-        with open(s3_filename, "rb") as f:
+        with open(s3_filename, "wb") as f:
             f.write(content)
         s3.upload_file(s3_filename, bucket, s3_filename)
         logger.info("File has been successfully uploaded")
@@ -63,12 +38,10 @@ def lambda_handler(event, context):
     if not s3_filename:
         raise ValueError("The S3 filename is not set")
 
-    content = b"Example content to be uploaded to S3"
+    content = event["content"]
 
-    # Connect to the RDS PostgreSQL instance
     try:
         create_file(logger, s3_bucket, s3_filename, content)
-        send_sns_notification(logger, s3_bucket, s3_filename)
 
         return {
             'statusCode': 200,
